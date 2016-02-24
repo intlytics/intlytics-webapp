52cafe99bc475e3e3abe384797717f2a16ce77dd
rangertaha <tahasaidi@gmail.com>
2016-02-23 23:20:49 -0500

    Updates

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var filters = require('gulp-filter');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+var pkg = require('./package.json');
+var dirs = pkg['intlytics-configs'].directories;
+
+
+gulp.task('js', function () {
+   gulp.src([
+       './node_modules/angular/angular.js',
+       './node_modules/angular-route/angular-route.js'
+   ])
+   .pipe(concat('angular.bundle.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build', ['js', 'sass'], function () {
+    var filter = filters([
+        'components/**/*',
+        'view1/**/*',
+        'view2/**/*',
+        '*.html',
+        '*.js',
+        '!src/sass'
+    ]);
+    gulp.src(['src/**/*'])
+        .pipe(filter)
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['build'],function () {
+  gulp.watch('./src/app/!*.scss', ['sass']);
+  gulp.watch('./src/app/!*.js', ['js']);
+  gulp.watch('./src/app/!*.html', ['html']);
+});
+
+gulp.task('default', ['build']);
